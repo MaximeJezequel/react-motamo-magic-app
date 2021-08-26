830cb6fae4528d1c9cb472c48d086c62a14cec5b
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-08-27 00:49:04 +0200

    inversion maison/valise corrected

--- src/App.js
+++ src/App.js
@@ -4,43 +4,45 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const motamo =
+  ['acteur', 'feutre', 'dessin', 'agenda', 'cadran', 'sudoku', 'casino', 'espace', 'danger', 'quiche',
+  'guidon', 'cerise', 'zénith', 'étoile', 'saphir', 'fiesta', 'sketch', 'argent', 'chaton', 'ananas',
+  'cheval', 'diable', 'citron', 'tomate', 'erreur', 'sphère', 'whisky', 'crypte', 'animal', 'zombie',
+  'donjon', 'beauté', 'nectar', 'puzzle', 'pédalo', 'basket', 'médium', 'italie', 'jaunir', 'karaté',
+  'parfum', 'banane', 'maison', 'valise', 'humour', 'idylle', 'jambon', 'oiseau', 'boxeur', 'hot-dog',
+  'litchi', 'orange', 'brunch', 'rythme', 'moulin', 'voyage','bonzaï', 'piment', 'loisir','projet']
 
-const motamo =
-['acteur', 'feutre', 'dessin', 'agenda', 'cadran', 'sudoku', 'casino', 'espace', 'danger', 'quiche',
-'guidon', 'cerise', 'zénith', 'étoile', 'saphir', 'fiesta', 'sketch', 'argent', 'chaton', 'ananas',
-'cheval', 'diable', 'citron', 'tomate', 'erreur', 'sphère', 'whisky', 'crypte', 'animal', 'zombie',
-'donjon', 'beauté', 'nectar', 'puzzle', 'pédalo', 'basket', 'médium', 'italie', 'jaunir', 'karaté',
-'parfum', 'banane', 'valise', 'maison', 'humour', 'idylle', 'jambon', 'oiseau', 'boxeur', 'hot-dog',
-'litchi', 'orange', 'brunch', 'rythme', 'moulin', 'voyage','bonzaï', 'piment', 'loisir','projet']
+  let [letter, setLetter] = useState(0);
+  let [total, setTotal] = useState(0);
 
-let [letter, setLetter] = useState(0);
-let [total, setTotal] = useState(0);
-
-const handleYes = () => {
-  if (letter < 6){
-    setLetter(letter+1);
-    setTotal(total+Math.pow(2,5-letter));
+  const handleYes = () => {
+    if (letter < 6){
+      setLetter(letter+1);
+      setTotal(total+Math.pow(2,5-letter));
+    }
   }
-}
 
-const handleNo = () => {
-  if (letter < 6){
-    setLetter(letter+1);
+  const handleNo = () => {
+    if (letter < 6){
+      setLetter(letter+1);
+    }
   }
-}
 
-const handleReset = () => {
-  setLetter(0);
-  setTotal(0);
-}
+  const handleReset = () => {
+    setLetter(0);
+    setTotal(0);
+  }
 
-const wipeHistory = () => {
-  window.location.replace(`https://www.google.com/search?q=${motamo[total-1]}`)
-}
+  const wipeHistory = () => {
+    let google = `https://www.google.com/search?q=${motamo[total-1]}`
+    // let googleImg = `https://www.google.com/search?q=${motamo[total-1]}&tbm=isch`
+    // let wikipedia = `https://fr.wikipedia.org/wiki/${motamo[total-1]}`
+    navigator.clipboard.writeText('')
+    window.location.replace(google)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
         {letter < 6
           ? <div className = "total">{letter} ({total})</div>
           : <div className = "total" onClick={wipeHistory}>
@@ -57,7 +59,6 @@ const wipeHistory = () => {
           }
         </div>
         <div onClick={handleReset}>RESET</div>
-      </header>
     </div>
   );
 }
