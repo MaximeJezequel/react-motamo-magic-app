e5b9041ddc82e41c7ef376a7f02bbad9cadd78af
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-09-27 12:25:43 +0200

    footer css added + choice all vs img in header

--- src/Google.js
+++ src/Google.js
@@ -14,6 +14,7 @@ const Google = () => {
 
     let [letter, setLetter] = useState(0);
     let [total, setTotal] = useState(0);
+    let [url, setUrl]= useState(0)
 
     const handleYes = () => {
         if (letter < 6){
@@ -21,20 +22,29 @@ const Google = () => {
             setTotal(total+Math.pow(2,5-letter));
         }
     }
-    
     const handleNo = () => {
         if (letter < 6){
             setLetter(letter+1);
         }
     }
-
     const handleReset = () => {
         setLetter(0);
         setTotal(0);
     }
+    const handleAll = () => {
+        setUrl(0);
+    }
+    const handleImg = () => {
+        setUrl(1);
+    }
+    
+
 
     const wipeHistory = () => {
-        let google = `https://www.google.com/search?q=${motamo[total-1]}`
+        let google = "https://www.google.com/search?q="
+        url === 0
+            ? google = `https://www.google.com/search?q=${motamo[total-1]}`
+            : google = `https://www.google.com/search?q=${motamo[total-1]}&tbm=isch`
         
         total > 0 && total < 61 &&
         window.location.replace(google)
@@ -45,8 +55,8 @@ const Google = () => {
             <div className = 'google-header'>
                 <div className="google-headerleft">
                     <svg className="google-headerleftsvg"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
-                    <p className="google-headerlefttitle active" >TOUS</p>
-                    <p className="google-headerlefttitle">IMAGES</p>
+                    <p className={url === 0 ? "google-headerlefttitle active" : "google-headerlefttitle"} onClick={handleAll}>TOUS</p>
+                    <p className={url === 1 ? "google-headerlefttitle active" : "google-headerlefttitle"} onClick={handleImg}>IMAGES</p>
                 </div>
                 <div className="google-headerright">
                     <svg className="gb_6e" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
@@ -57,7 +67,9 @@ const Google = () => {
                 <img src="https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png" alt="" onClick={handleReset}/>
                 <div class="form-group">
                     <input type="texte">
-                    
+                    {/* <svg focusable="false" viewBox="0 0 24 24">
+                        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                    </svg> */}
                     {/* <label><i class="fas fa-microphone"></i></label> */}
                     </input>
                 </div>
@@ -82,7 +94,16 @@ const Google = () => {
                     <div className = 'ghostRight' onClick={handleNo}></div>
                 </>
             }
-            {/* <div className="footer">France</div> */}
+            {/* <div className = "empty"></div> */}
+            <div className="footer">
+                <div className="footer-row">France</div>
+                <div className="footer-3col">
+                    <div className="footer-col">Paramètres</div>
+                    <div className="footer-col">Confidentialité</div>
+                    <div className="footer-col">Conditions</div>
+                </div>
+            </div>
+
         </div>
     );
 }
