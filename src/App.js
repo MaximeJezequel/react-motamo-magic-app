eb92428192c6ee752e4c3550fb5669f938918c6b
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-10-01 20:01:42 +0200

    merge dev

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import React from 'react';
+import React from "react"
 
 // import Black from './Black'
-import Google from './Google'
+import Google from "./Google"
 
-import './App.css';
+import "./App.css"
 
 function App() {
-
-  return (
-<<<<<<< HEAD
-    <div className="App">
-        {letter < 6
-          ? <button className = "total">{letter} ({total})</button>
-          : <button className = "total" onClick={wipeHistory}>
-              {total > 0 && total < 61 &&
-                motamo[total-1].toUpperCase()} ({total})
-            </button>
-        }
-        <div className = "swipe">
-          {letter < 6 &&
-            <>
-              <button className = "touch" onClick={handleYes}></button>
-              <button className = "touch" onClick={handleNo}></button>
-            </>
-          }
-        </div>
-        <button className = "reset" onClick={handleReset}>RESET</button>
-    </div>
-=======
-  <>
-    <Google />
-    {/* <Black /> */}
-  </>
->>>>>>> dev
-  );
+	return (
+		<>
+			<Google />
+			{/* <Black /> */}
+		</>
+	)
 }
 
-export default App;
+export default App
